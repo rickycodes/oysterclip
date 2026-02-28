40784c704b491f3850dc16f19525e6b45ae6bd02
rickycodes <ricky.miller@gmail.com>
2026-02-28 06:15:36 -0500

    Add title

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,15 @@ mod common;
 
 use app::App;
 use dioxus::desktop::Config as DesktopConfig;
+use dioxus::desktop::tao::window::WindowBuilder;
 use dioxus::prelude::LaunchBuilder;
 
 fn main() {
     LaunchBuilder::desktop()
-        .with_cfg(DesktopConfig::new().with_menu(None))
+        .with_cfg(
+            DesktopConfig::new()
+                .with_window(WindowBuilder::new().with_title("Clipboard Viewer"))
+                .with_menu(None),
+        )
         .launch(App);
 }
